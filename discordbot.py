4493279ef740738879b3c5f561bd894876709092
rgbseven <75620611+rgbseven@users.noreply.github.com>
2020-12-07 20:55:28 +0800

    Add files via upload

---
+++ discordbot.py
@@ -0,0 +1,30 @@
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("ready")
+    game = discord.Game("Developer :: RGBSEVEN#8410")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("!status"):
+        await message.channel.send("`Bot is currently Online`")
+    if message.content.startswith("!developer"):
+        await message.channel.send("`RGBSEVEN#8410`")
+
+    if message.content.startswith("/warn"):
+        channel = message.content[6:24]
+        msg = message.content[25:]
+        await client.get_channel(int(channel)).send(msg)
+
+
+
+
+
+client.run("Nzg1NDI4MTc5MjQxNjY0NTEy.X83s_g.tV7N2T0x8MBpi6MyarjS3mhHOHA")
